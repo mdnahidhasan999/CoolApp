3851b096a068a04e069ab0fe75f112b130aae834
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-10-08 00:10:16 +0600

    commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo Nahid',
+      home: MatchListScreen(),
+    );
+  }
+}
+class MatchListScreen extends StatefulWidget {
+  const MatchListScreen({super.key});
+
+  @override
+  State<MatchListScreen> createState() => _MatchListScreenState();
+}
+
+class _MatchListScreenState extends State<MatchListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Match List'),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
+
