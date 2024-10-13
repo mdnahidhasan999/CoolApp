ee8b6f04f87fd26fcbe4a0e4dacbc7639d1d1d27
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-10-13 19:44:26 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,23 @@
-import 'package:coolapp/firebase_notification.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseNotification.instance.initialize();
-
-  runApp(const MyApp());
+void main(){
+  runApp(const GoogleMapsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo Nahid',
-      home: MatchListScreen(),
-    );
-  }
-}
-
-class MatchListScreen extends StatefulWidget {
-  const MatchListScreen({super.key});
-
-  @override
-  State<MatchListScreen> createState() => _MatchListScreenState();
-}
-
-class _MatchListScreenState extends State<MatchListScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Match List'),
-        backgroundColor: Colors.blue,
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
       ),
     );
   }
 }
+
+
+
